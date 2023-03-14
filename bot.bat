f1d9e318a19de2fe43e0495a6f8e4c2a5232bebb
alaanvv <valealan84@gmail.com>
2023-03-14 20:57:02 -0300

    Now the bot clean the embeds whe stop running by ctrlC

--- bot.bat
+++ bot.bat
@@ -1,4 +1,2 @@
-@REM It runs the bot and the lavalink in separated windows,
-@REM you can execute lavalink only with lavalink.bat
-start /max cmd /k call npm start
+start /max cmd /k "echo Press CTRL+C to exit & npm start"
 start /max cmd /k call java -jar lavalink/Lavalink.jar
